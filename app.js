7fb017368ecb808b2c5dda7635a20e517f86d029
kbroz1 <kmbroz@gmail.com>
2021-06-02 20:33:54 -0600

    removed day of week alert

--- app.js
+++ app.js
@@ -155,31 +155,31 @@
 
 //switch statement to return the current day
 
-var day;
-switch (new Date().getDay()) {
-    case 0:
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Monday";
-        break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-    case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "Friday";
-        break;
-    case 6:
-        day = "Saturday";
+// var day;
+// switch (new Date().getDay()) {
+//     case 0:
+//         day = "Sunday";
+//         break;
+//     case 1:
+//         day = "Monday";
+//         break;
+//     case 2:
+//         day = "Tuesday";
+//         break;
+//     case 3:
+//         day = "Wednesday";
+//         break;
+//     case 4:
+//         day = "Thursday";
+//         break;
+//     case 5:
+//         day = "Friday";
+//         break;
+//     case 6:
+//         day = "Saturday";
 
-}
-alert(day);
+// }
+// alert(day);
 
 
 
